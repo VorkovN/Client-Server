a2d978cee1d7905a03c104de8151e48982be6e7d
VorkovN <57640795+VorkovN@users.noreply.github.com>
2020-05-06 02:56:10 +0500

    v 3.1

--- Client-Server/src/Server/Server.java
+++ Client-Server/src/Server/Server.java
@@ -15,21 +15,6 @@ public class Server {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         MyCollection myCollection = new MyCollection();
 
-        HelpCommand helpCommand = new HelpCommand();
-        InfoCommand infoCommand = new InfoCommand();
-        ShowCommand showCommand = new ShowCommand();
-        SaveCommand saveCommand = new SaveCommand();
-        AddCommand addCommand = new AddCommand();
-        UpdateCommand updateCommand = new UpdateCommand();
-        RemoveByIdCommand removeByIdCommand = new RemoveByIdCommand();
-        ClearCommand clearCommand = new ClearCommand();
-        ExecuteScriptCommand executeScriptCommand = new ExecuteScriptCommand();
-        RemoveGreaterCommand removeGreaterCommand = new RemoveGreaterCommand();
-        CountLessThanDistanceCommand countLessThanDistance = new CountLessThanDistanceCommand();
-        FilterGreaterThanDistanceCommand filterGreaterThanDistance = new FilterGreaterThanDistanceCommand();
-        RemoveAllByDistanceCommand removeAllByDistanceCommand = new RemoveAllByDistanceCommand();
-        RemoveFirstCommand removeFirstCommand = new RemoveFirstCommand();
-
         System.out.println("Server is started");
         while(true){
             SocketAddress adress = new InetSocketAddress(2281);
@@ -41,7 +26,6 @@ public class Server {
                     Command cmd = (Command) fromClient.readObject();
                     cmd.setMyCollection(myCollection);
                     toClient.writeObject(cmd.execute());
-                    //toClient.writeObject(((Command) fromClient.readObject()).execute());
                 }
             } catch (ClassNotFoundException | IOException ignored) {}
         }
